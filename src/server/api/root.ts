2db7fab0bad2e0b658f3774fb9c6e7a2514e3821
Lok Lok <yellowhappy831@gmail.com>
2025-09-20 20:14:33 +0800

    save

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,6 @@
 // import { postRouter } from "@/server/api/routers/post";
 import { createCallerFactory, createTRPCRouter } from "@/server/api/trpc";
-import { userDataRouter } from "./routers/UserDataRouter";
+import { userDataRouter } from "./routers/UserData";
 
 /**
  * This is the primary router for your server.
