40a0395bed596a1e070d55775cba6511cc125c5f
Lok Lok <yellowhappy831@gmail.com>
2025-09-21 11:28:38 +0800

    save

--- src/components/mini-apps/Objects/RTextureMat.tsx
+++ src/components/mini-apps/Objects/RTextureMat.tsx
@@ -1,41 +1,7 @@
-import { easing, geometry } from "maath";
 import { Suspense, useEffect, useMemo, useRef } from "react";
-import { signal } from "@preact/signals-core";
-import {
-    Cloud,
-    Environment,
-    MeshPortalMaterial,
-    RenderTexture,
-    Sky,
-    Sphere,
-    useEnvironment,
-    useGLTF,
-} from "@react-three/drei";
+import { useEnvironment } from "@react-three/drei";
 import { Canvas, createPortal, extend, useFrame } from "@react-three/fiber";
-import {
-    Root,
-    Container,
-    Text,
-    setPreferredColorScheme,
-    Content,
-    Fullscreen,
-    Input,
-} from "@react-three/uikit";
-import { BellRing, Check } from "@react-three/uikit-lucide";
-import {
-    Defaults,
-    colors,
-    Avatar,
-    Button,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-    Switch,
-} from "@react-three/uikit-default";
-import { Avatar2 } from "./Avatar2";
-import { LinearToSRGB } from "three/src/math/ColorManagement.js";
+
 import {
     NoColorSpace,
     Object3D,
