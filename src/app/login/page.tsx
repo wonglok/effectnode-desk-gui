9fd6a1074f82965741d718183a364018386dd0ba
Lok Lok <yellowhappy831@gmail.com>
2025-09-21 18:25:08 +0800

    save

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
     AxeIcon,
     GalleryVerticalEnd,
--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -7,6 +7,7 @@ import { LogInIcon } from "lucide-react";
 import { signIn, useSession } from "next-auth/react";
 import { useEffect } from "react";
 import { redirect } from "next/navigation";
+import Link from "next/link";
 
 export function LoginForm({
     className,
@@ -60,35 +61,40 @@ export function LoginForm({
                     </span>
                 </div> */}
                 {/*  */}
-
-                <Button
-                    onClick={() => {
-                        // signIn("google", {
-                        //     redirect: true,
-                        //     redirectTo: `/desk`,
-                        // });
-                        // signIn(undefined, {
-                        //     redirect: true,
-                        //     redirectTo: "/desk",
-                        // });
-                        signIn();
-                        //
-                        //
-                        // location.assign(`/api/auth/signin`);
-                        //
-                        //
-                    }}
-                    variant="outline"
-                    className="w-full cursor-pointer"
-                >
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                        <path
-                            d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
-                            fill="currentColor"
-                        />
-                    </svg>
-                    Login Google
-                </Button>
+                <Link href={`/api/auth/signin`}>
+                    <Button
+                        onClick={() => {
+                            // signIn("google", {
+                            //     redirect: true,
+                            //     redirectTo: `/desk`,
+                            // });
+                            // signIn(undefined, {
+                            //     redirect: true,
+                            //     redirectTo: "/desk",
+                            // });
+                            // signIn();
+                            // location.href = ;
+                            //
+                            //
+                            // location.assign(`/api/auth/signin`);
+                            //
+                            //
+                        }}
+                        variant="outline"
+                        className="w-full cursor-pointer"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                        >
+                            <path
+                                d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+                                fill="currentColor"
+                            />
+                        </svg>
+                        Login Google
+                    </Button>
+                </Link>
             </div>
             {/* <div className="text-center text-sm">
                 Don&apos;t have an account?{" "}
