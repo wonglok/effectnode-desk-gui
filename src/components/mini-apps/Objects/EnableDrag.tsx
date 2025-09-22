558c0ed7f20f976ade52e296d210626e3b215851
Lok Lok <yellowhappy831@gmail.com>
2025-09-22 21:56:46 +0800

    save

--- src/components/mini-apps/Objects/EnableDrag.tsx
+++ src/components/mini-apps/Objects/EnableDrag.tsx
@@ -3,18 +3,34 @@ import { createPortal, useThree } from "@react-three/fiber";
 import { useEffect, useMemo, useRef, useState } from "react";
 import { Matrix4, Object3D, Vector3 } from "three";
 import md5 from "md5";
+import { v4 } from "uuid";
 
 let keyname = `position_store_${md5(`${process.env.APP_NAME}`)}}`;
-let isDown = { current: false };
+let isDown: any = { current: "" };
+
+let st = new Object3D();
+let lt = new Object3D();
+let nt = new Object3D();
+let dt = new Object3D();
+let at = new Object3D();
 
 export function EnableDrag({
     name = "drag",
     children,
     initPos = [0, 0, 0],
 }: any) {
+    let myRand = useMemo(() => {
+        return `${v4()}`;
+    }, []);
     let controls: any = useThree((r) => r.controls);
 
-    let [matrix, setMatrix] = useState(new Matrix4());
+    let [o3API, setO3] = useState(() => {
+        let o3 = new Object3D();
+        return {
+            o3,
+            display: <primitive object={o3}></primitive>,
+        };
+    });
     let [ready, setReady] = useState(false);
     let store = useMemo(() => {
         let tt: any;
@@ -62,42 +78,62 @@ export function EnableDrag({
 
     return (
         <>
-            {ready && matrix && (
+            {ready && (
                 <DragControls
-                    matrix={matrix}
+                    autoTransform={false}
                     axisLock="y"
                     onDragStart={() => {
-                        if (isDown.current) {
-                            return;
+                        if (!isDown.current) {
+                            isDown.current = myRand;
                         }
+
+                        st.position.copy(o3API.o3.position);
 
                         if (controls) {
                             controls.enabled = false;
                         }
-
-                        isDown.current = true;
                     }}
-                    onDrag={() => {
-                        if (isDown.current) {
-                            store.setItem(
-                                name,
-                                JSON.parse(JSON.stringify(matrix.toArray())),
-                            );
+                    onDrag={(lm, dlm, wm, dwm) => {
+                        // store.setItem(name, JSON.parse(JSON.stringify(lm)));
+
+                        if (isDown.current === myRand) {
+                            lm.decompose(nt.position, nt.quaternion, nt.scale);
+
+                            console.log(nt.position);
+                            if (lt.position.length() === 0) {
+                                lt.position.copy(nt.position);
+                            }
+                            dt.position.copy(nt.position).sub(lt.position);
+
+                            lt.position.copy(nt.position);
+
+                            at.position.add(dt.position);
+
+                            o3API.o3.position.add(dt.position);
                         }
                     }}
                     onDragEnd={() => {
                         store.setItem(
                             name,
-                            JSON.parse(JSON.stringify(matrix.toArray())),
+                            JSON.parse(
+                                JSON.stringify(o3API.o3.position.toArray()),
+                            ),
                         );
+
+                        lt.position.multiplyScalar(0);
+                        dt.position.multiplyScalar(0);
+                        st.position.multiplyScalar(0);
+                        nt.position.multiplyScalar(0);
+                        at.position.multiplyScalar(0);
 
                         if (controls) {
                             controls.enabled = true;
                         }
-                        isDown.current = false;
+                        isDown.current = "";
                     }}
                 >
-                    {children}
+                    {createPortal(<>{children}</>, o3API.o3)}
+                    {o3API.display}
                 </DragControls>
             )}
         </>
