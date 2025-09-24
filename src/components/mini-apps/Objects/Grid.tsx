9ea02786a63d6b2bb323b0e2b5bb7221c77a1e99
Lok Lok <yellowhappy831@gmail.com>
2025-09-24 17:33:37 +0800

    save

--- src/components/mini-apps/Objects/Grid.tsx
+++ src/components/mini-apps/Objects/Grid.tsx
@@ -1,47 +1,56 @@
 import { Instance, Instances, Plane } from "@react-three/drei";
+import { useThree } from "@react-three/fiber";
+import { useEffect } from "react";
 import { Color } from "three";
 
-export const Grid = ({ number = 23, lineWidth = 0.036, height = 0.5 }) => {
+let primary = new Color("#00eeee").offsetHSL(0.0, 0.0, 0.05);
+let grid = new Color(primary).offsetHSL(0, 0, -0.3);
+let background = new Color(primary).offsetHSL(0, 0, -0.44);
+
+export const Grid = ({ number = 25, lineWidth = 0.036, height = 0.5 }) => {
     // Renders a grid and crosses as instances
+    let scene = useThree((r) => r.scene);
+
+    useEffect(() => {
+        scene.background = background;
+    }, [background]);
+
+    let insts = [];
+
+    for (let y = -number; y <= number; y++) {
+        for (let x = -number; x <= number; x++) {
+            insts.push(
+                <group key={`${x}:${y}`} position={[x, 0, y]}>
+                    <Instance rotation={[-Math.PI / 2, 0, 0]} />
+                    <Instance rotation={[-Math.PI / 2, 0, Math.PI / 2]} />
+                </group>,
+            );
+        }
+    }
+
     return (
         <>
             <Instances position={[0, 0.0, 0]}>
                 <planeGeometry args={[lineWidth, height]} />
-                <meshBasicMaterial color="#0ff" />
+                <meshBasicMaterial color={primary} />
 
-                {Array.from({ length: number }, (_, y) =>
-                    Array.from({ length: number }, (_, x) => (
-                        <group
-                            key={x + ":" + y}
-                            position={[
-                                x * 2 - Math.floor(number / 2) * 2,
-                                0.001,
-                                y * 2 - Math.floor(number / 2) * 2,
-                            ]}
-                        >
-                            <Instance rotation={[-Math.PI / 2, 0, 0]} />
-                            <Instance
-                                rotation={[-Math.PI / 2, 0, Math.PI / 2]}
-                            />
-                        </group>
-                    )),
-                )}
+                {insts}
 
                 <gridHelper
-                    args={[50, 50, "#cff", "#cff"]}
+                    args={[50, 50, grid, grid]}
                     position={[0, -0.01, 0]}
                 />
-
-                <Plane
-                    scale={50}
-                    rotation={[-Math.PI * 0.5, 0, 0]}
-                    position={[0, -0.1, 0]}
-                    receiveShadow
-                    castShadow
-                >
-                    <meshBasicMaterial></meshBasicMaterial>
-                </Plane>
             </Instances>
+
+            <Plane
+                scale={50}
+                rotation={[-Math.PI * 0.5, 0, 0]}
+                position={[0, -0.1, 0]}
+                receiveShadow
+                castShadow
+            >
+                <meshBasicMaterial color={background}></meshBasicMaterial>
+            </Plane>
         </>
     );
 };
